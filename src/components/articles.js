ca958ae772a0f16aa24732194beb5cc0cd5ea62a
Ted Osuch <ted@osuch.com>
2019-11-11 20:13:44 +0000

    renamed css class to className

--- src/components/articles.js
+++ src/components/articles.js
@@ -6,11 +6,11 @@ const Articles = ({ articles }) => {
       <center><h1>Article List</h1></center>
     <center><em>&nbsp;--&nbsp;Link here for <a href="https://documenter.getpostman.com/view/9438658/SW7T6qef" target="_blank" rel="noopener noreferrer">API docs</a>--</em></center>
       {articles.map((article) => (
-        <div class="card">
-          <div class="card-body">
-            <h2 class="card-title">{article.title}</h2>
-            <h3 class="card-subtitle mb-2 text-muted">{article.body}</h3>
-            <h6 class="card-subtitle mb-2 text-muted">{article.superhero}</h6>
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">{article.title}</h2>
+            <h3 className="card-subtitle mb-2 text-muted">{article.body}</h3>
+            <h6 className="card-subtitle mb-2 text-muted">{article.superhero}</h6>
           </div>
         </div>
       ))}
